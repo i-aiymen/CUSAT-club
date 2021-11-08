2145d3c1ea712135b527876dc798aa9a9be37f5f
MUHAMMED AYIMEN <aiymenlatheef03@gmail.com>
2021-11-08 22:59:44 +0530

    Updated: Upcoming events with eventbrite links

--- cusat_club_website/src/components/eventDesktop/Up.js
+++ cusat_club_website/src/components/eventDesktop/Up.js
@@ -18,9 +18,9 @@ const Up = () => {
 
         <>
 			<Carousel breakPoints={breakPoints}>
-				<img src={Session21} alt="poster" className={css(styles.poster)} />
-				<img src={Session20} alt="poster" className={css(styles.poster)} />
-				<img src={Session19} alt="poster" className={css(styles.poster)} />
+				<a href="https://www.eventbrite.com/e/tips-to-crack-civil-service-exams-by-an-ias-officer-tickets-205488290007?aff=ebdsoporgprofile"><img src={Session21} alt="poster" className={css(styles.poster1)} /></a>
+				<a href="https://www.eventbrite.com/e/life-of-a-technical-program-manager-tickets-194422010467?aff=ebdsoporgprofile"><img src={Session20} alt="poster" className={css(styles.poster1)} /></a>
+				<a href="https://www.eventbrite.com/e/job-opportunities-in-mechanical-engineering-tickets-190973826857?aff=ebdsoporgprofile"><img src={Session19} alt="poster" className={css(styles.poster1)} /></a>
 			</Carousel>
 			<div className={css(styles.thirdRow)}>
             <div className={css(styles.subtTitle)}>
@@ -57,6 +57,33 @@ const styles = StyleSheet.create({
 		minHeight: 300,
 		borderRadius: 15,
 		cursor: "pointer"
+	},
+	poster1: {
+		':hover': {
+			opacity: "0.8",
+		},
+		
+		// margin: "0 25px",
+		width: 406,
+		height: 370,
+		borderRadius: 15,
+		cursor: "pointer",
+		'@media only screen and (max-width: 1870px)': {
+			width: 489,
+			height: 489,
+
+		},
+		'@media only screen and (max-width: 1680px)': {
+			width: 421,
+			height: 421,
+
+		},
+		'@media only screen and (max-width: 1500px)': {
+			width: 406,
+			height: 365,
+			
+
+		},
 	},
     thirdRow: {
 		display: "flex",
