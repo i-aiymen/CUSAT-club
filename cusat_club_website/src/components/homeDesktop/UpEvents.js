9fa1a645e01f7b6bb7819907c9cbeb5ba6abd003
MUHAMMED AYIMEN <aiymenlatheef03@gmail.com>
2021-11-08 23:25:53 +0530

    Updated: Upcoming events in main page

--- cusat_club_website/src/components/homeDesktop/UpEvents.js
+++ cusat_club_website/src/components/homeDesktop/UpEvents.js
@@ -1,10 +1,12 @@
 import { StyleSheet, css } from "aphrodite";
 import React from "react";
 import "../../arrow.css"
-import Poster from "../../assets/logo/event.png";
 import { COLORS } from "../../styles/Constants";
 import UPCOMING from "../../assets/icons/UPCOMING.png";
 import Carousel from "react-elastic-carousel";
+import Session19 from "../../assets/sessions/Session 19.jpg";
+import Session20 from "../../assets/sessions/Session 20.jpg";
+import Session21 from "../../assets/sessions/Session 21.jpg";
 
 const breakPoints = [
 	{ width: 1, itemsToShow: 1 },
@@ -24,12 +26,9 @@ const UpEvents = () => {
 						</div>
 					</div>
 					<Carousel breakPoints={breakPoints} showArrows={false}>
-						<img src={Poster} alt="poster" className={css(styles.poster)} />
-						<img src={Poster} alt="poster" className={css(styles.poster)} />
-						<img src={Poster} alt="poster" className={css(styles.poster)} />
-						<img src={Poster} alt="poster" className={css(styles.poster)} />
-						<img src={Poster} alt="poster" className={css(styles.poster)} />
-						<img src={Poster} alt="poster" className={css(styles.poster)} />
+						<a href="https://www.eventbrite.com/e/tips-to-crack-civil-service-exams-by-an-ias-officer-tickets-205488290007?aff=ebdsoporgprofile" target='_blank' rel="noopener noreferrer"><img src={Session21} alt="poster" className={css(styles.poster1)} /></a>
+						<a href="https://www.eventbrite.com/e/life-of-a-technical-program-manager-tickets-194422010467?aff=ebdsoporgprofile" target='_blank' rel="noopener noreferrer"><img src={Session20} alt="poster" className={css(styles.poster1)} /></a>
+						<a href="https://www.eventbrite.com/e/job-opportunities-in-mechanical-engineering-tickets-190973826857?aff=ebdsoporgprofile" target='_blank' rel="noopener noreferrer"><img src={Session19} alt="poster" className={css(styles.poster1)} /></a>
 					</Carousel>
 					<div className={css(styles.thirdRow)}>
 						<div className={css(styles.subtTitle)}>
@@ -59,8 +58,13 @@ const styles = StyleSheet.create({
 		display: "flex",
 		flexDirection: "row",
 		margin: "50px 35px 50px 35px",
+		marginLeft: 50,
 		justifyContent: "space-between",
 		alignItems: "center",
+		'@media only screen and (max-width: 1500px)': {
+			marginLeft: 35,
+
+		},
 	},
 	titleBox: {
 		width: "auto",
@@ -114,6 +118,34 @@ const styles = StyleSheet.create({
 		minHeight: 300,
 		borderRadius: 15,
 		cursor: "pointer"
+	},
+	poster1: {
+		':hover': {
+			opacity: "0.8",
+		},
+		
+		// margin: "0 25px",
+		width: 406,
+		height: 370,
+		borderRadius: 15,
+		cursor: "pointer",
+		'@media only screen and (max-width: 1870px)': {
+			
+			width: 489,
+			height: 489,
+
+		},
+		'@media only screen and (max-width: 1680px)': {
+			width: 421,
+			height: 421,
+
+		},
+		'@media only screen and (max-width: 1500px)': {
+			width: 406,
+			height: 365,
+			
+
+		},
 	},
 
 	thirdRow: {
